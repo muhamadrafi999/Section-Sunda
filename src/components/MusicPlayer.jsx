57906f2191cafb29956c0bac81284890592e668a
muhamadrafi999 <muhamadrafi@smkwikrama.sch.id>
2026-08-12 16:03:40 +0700

    update music

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,12 +1,8 @@
 import { useEffect, useRef } from "react";
-import {
-  HiSpeakerWave,
-  HiSpeakerXMark,
-} from "react-icons/hi2";
-
 import { useMusic } from "../context/MusicContext";
-
 import BackgroundMusic from "../assets/music/Maher_Zain-Rahmatun_LiAlameen.mp3";
+import VinylRecord from "../assets/images/vinyl-record.png";
+import Tonearm from "../assets/images/tonearm.png";
 
 function MusicPlayer() {
   const audioRef = useRef(null);
@@ -22,9 +18,9 @@ function MusicPlayer() {
     if (!audioRef.current) return;
 
     if (isPlaying) {
-      audioRef.current
-        .play()
-        .catch((error) => console.log(error));
+      audioRef.current.play().catch((error) => {
+        console.log("Audio belum dapat diputar:", error);
+      });
     } else {
       audioRef.current.pause();
     }
@@ -43,39 +39,75 @@ function MusicPlayer() {
         preload="auto"
       />
       <button
+        type="button"
         onClick={toggleMusic}
-        aria-label="Toggle background music"
+        aria-label={
+          isPlaying
+            ? "Matikan background music"
+            : "Nyalakan background music"
+        }
         className="
           fixed
           bottom-6
           right-6
-          z-9999
+          z-[9999]
           w-15
           h-15
-          rounded-full
-          bg-[#7D2424]
-          text-white
-          shadow-xl
-          flex
-          items-center
-          justify-center
-          transition-all
+          cursor-pointer
+          transition-transform
           duration-300
           hover:scale-110
           active:scale-95
-          cursor-pointer
         "
       >
-        <div
-          className={`flex items-center justify-center ${
-            isPlaying ? "animate-spin-slow" : ""
-          }`}
-        >
-          {isPlaying ? (
-            <HiSpeakerWave size={28} />
-          ) : (
-            <HiSpeakerXMark size={28} />
-          )}
+        <div className="relative w-full h-full">
+          <div
+            className="
+              absolute
+              left-1/2
+              top-1/2
+              w-14
+              h-14
+              -translate-x-1/2
+              -translate-y-1/2
+            "
+          >
+            <img
+              src={VinylRecord}
+              alt=""
+              className={`
+                w-full
+                h-full
+                object-contain
+                ${
+                  isPlaying
+                    ? "animate-[spin_2.5s_linear_infinite]"
+                    : ""
+                }
+              `}
+            />
+          </div>
+          <img
+            src={Tonearm}
+            alt=""
+            className={`
+              absolute
+              -right-3
+              -top-1
+              w-10
+              h-15
+              object-contain
+              origin-top-right
+              transition-transform
+              duration-500
+              ease-in-out
+              ${
+                isPlaying
+                  ? "rotate-[18deg]"
+                  : "rotate-[-8deg]"
+              }
+            `}
+          />
         </div>
       </button>
     </>
